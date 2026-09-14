3eb492c6acdf1d9ef86634f53d5207ca4edb5888
JBIZZLE-goat <jwbrown0524@gmail.com>
2026-09-14 15:36:52 -0500

    Build Hero class

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 from goblin import Goblin
-
+from hero import Hero
 
 ARENA_NAME = "Jonathons Arena of Doom"
 
@@ -14,12 +14,19 @@ def main():
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
 
-    newGoblin = Goblin("Scribble")
+    newGoblin = Goblin("Regan")
 
     print(f"{newGoblin.name} enters the arena with {newGoblin.health} health.")
 
-print("But no hero has answered the call... yet.")
-
+    hero = Hero("Mr. Speak")
+    print(f"{hero.name} enters the Jonathons Arena of Doom with {hero.health} health.")
+    print(f"{hero.name} has leveled up and became the {hero.hero_class}.")
+    hero_attack = hero.attack()
+    print(f"{hero.name} attacks {goblin.name} for {hero_attack} damage.")
+    goblin.take_damage(hero_attack)
+    goblin_attack = goblin.attack()
+    print(f"{goblin.name} attacks {hero.name} for {goblin_attack} damage.")
+    hero.take_damage(goblin_attack)
 
 if __name__ == "__main__":
     main()
--- hero.py
+++ hero.py
@@ -1,4 +1,22 @@
+import random
+
 class Hero:
     """The hero blueprint will be implemented later in the project."""
 
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.health = 110
+        self.attack_power = 17
+        self.hero_class = "Cyber Sorcerer"
+        
+    def attack(self):
+        """Return a random value from 1 through this Hero's attack power."""
+        return random.randint(1, self.attack_power) 
+
+    def take_damage(self, damage):
+        """Subtract damage, but do not allow health to fall below zero."""
+       
+    def is_alive(self):
+        """Return a Boolean based on this Hero's health.""" 
+
+  
